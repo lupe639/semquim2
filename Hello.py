866b1f04a7a2e82360d6e2f3e12b018d1aa7da7b
lupe639 <146982271+lupe639@users.noreply.github.com>
2023-10-04 11:39:05 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.ndimage.interpolation import zoom
 from streamlit_drawable_canvas import st_canvas
 from utils import process_image
-st.markdown("# Digit :blue[Recognition] :green[App] :pencil: 💻🤖")
+st.markdown("# :blue[Aplicación] para reconocimiento de :red[números] :pencil: 💻🤖")
 
 # Load trained model
 model = tf.keras.models.load_model('mi_modelo.h5')
